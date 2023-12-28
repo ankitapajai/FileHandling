7727121138cca2fdd560713f43cf2dfa015c7e36
ankitapajai <ankitapajai@gmail.com>
2023-12-28 13:49:10 +0530

    create file

--- src/main/java/com/bootcoding/fileHandling/practiceOnFileHandling/CreateFile_Ex1.java
+++ src/main/java/com/bootcoding/fileHandling/practiceOnFileHandling/CreateFile_Ex1.java
@@ -6,13 +6,13 @@ import java.io.IOException;
 public class CreateFile_Ex1 {
     public static void main(String[] args) {
 
-        File file = new File("D:\\programs\\FileOperationEx2.csv");
+        File file = new File("D:\\programs\\FileOperationEx3.js");
 
         try{
             if(file.createNewFile()){
                 System.out.println("file " + file.getName() + " is created successfully!..");
             }else{
-                System.out.println("file is already exist");
+                System.out.println("file is already exist in directory");
             }
 
         }catch (IOException ex){
