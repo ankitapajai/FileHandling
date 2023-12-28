a40a00fa3148217ed105572fa3bc24cfff07df4f
ankitapajai <ankitapajai@gmail.com>
2023-12-28 13:51:30 +0530

    -

--- src/main/java/com/bootcoding/fileHandling/File/FileEx1.java
+++ src/main/java/com/bootcoding/fileHandling/File/FileEx1.java
@@ -1,2 +1,36 @@
-package com.bootcoding.fileHandling.File;public class FileEx1 {
+package com.bootcoding.fileHandling.File;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class FileEx1 {
+    public static void main(String[] args) {
+
+        File file = new File("C:\\programs\\dd.txt");
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        
+
+        try {
+            if (!file.exists()) {
+                file.createNewFile();
+            } else {
+                file.delete();
+            }
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
+
+
+
