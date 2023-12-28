0881c49eff27988147e0d5844224af236507a37d
ankitapajai <ankitapajai@gmail.com>
2023-12-28 13:50:56 +0530

    -

--- src/main/java/com/bootcoding/fileHandling/directory/DirectoryListEx1.java
+++ src/main/java/com/bootcoding/fileHandling/directory/DirectoryListEx1.java
@@ -1,2 +1,32 @@
-package com.bootcoding.fileHandling.directory;public class DirectoryListEx1 {
+package com.bootcoding.fileHandling.directory;
+
+import java.io.File;
+
+public class DirectoryListEx1 {
+    public static void main(String[] args) {
+
+        new DirectoryListEx1().listAllFolders("D:\\codeSoft");
+    }
+
+    private void listAllFolders(String directoryPath) {
+        File file = new File(directoryPath);
+        if (file.isDirectory()) {
+            String[] list = file.list();
+            File[] files = file.listFiles();
+            for (File f : files) {
+                if (!f.getName().startsWith("")) ;
+                if (f.isDirectory()) {
+                    listAllFolders(f.getPath());
+                } else {
+                    print(f);
+                }
+            }
+        }else{
+            print(file);
+        }
+    }
+
+    private void print(File file) {
+    System.out.println(file.getPath() + "/" + file.getName());
+    }
 }
